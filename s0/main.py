e6fedffc0129990461c58d1e582dbc5fb352e7e1
iam-nazmul <nazmul.cse48@gmail.com>
2026-08-31 13:59:18 +0600

    ClickHouse database

--- s0/main.py
+++ s0/main.py
@@ -12,3 +12,16 @@ print("Connected!")
 print("ClickHouse version:", result.result_rows[0][0])
 result = client.query("SELECT 1")
 print("Query result:", result.result_rows[0][0])
+
+
+client.command("""
+    CREATE DATABASE IF NOT EXISTS demo
+""")
+
+print("Database created!")
+
+query = client.query("""
+SHOW DATABASES;
+""")
+
+print("Databases:", [row[0] for row in query.result_rows])
